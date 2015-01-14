e78d58d33e219bea3a538fa8a2cfe893d4f22a27
Tic-Toc Studio <Tic-Toc Studio@Gateway-PC>
2015-01-14 16:33:53 -0600

    se agrego un alert en el archiv js y se vinculo al documento index.html

--- js/scripts.js
+++ js/scripts.js
@@ -3,5 +3,5 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
-
+//el mensaje de prueba funciona bien, quiere decir que el archivo esta isto para incluir codigo JS
+//alert("mensaje de prueba");
